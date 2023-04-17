79c7245005a3de4de563ac6a596c84c2ac514fcd
Paul0522 <paul.milcape@gmail.com>
2023-04-17 16:28:40 -0500

    Commit numero dos

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # programa de python 
+import os
+import sys
+
+DEFAULT_FILENAME = "word.txt"
+
+def sort_list(item, ascending=True):
+    if not isinstance(items, list):
+        raise RuntimeError(f"No puede ordenar {type(items)}")
+    
+    return sorted(items, reverse=(not ascending))
